d2e11d82ec159bddc9e75dad2880ddade7ac9ef3
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2025-01-01 11:43:50 +0530

    done:vid020

--- src/app/header/main-menu/main-menu.component.ts
+++ src/app/header/main-menu/main-menu.component.ts
@@ -1,12 +1,20 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'main-menu',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './main-menu.component.html',
-  styleUrl: './main-menu.component.css'
+  styleUrl: './main-menu.component.css',
 })
 export class MainMenuComponent {
-
+  mainMenuItems: string[] = [
+    'Home',
+    'Products',
+    'Sale',
+    'New Arrival',
+    'Contact',
+    'Services',
+  ];
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from "./search/search.component";
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'product-list',
   standalone: true,
-  imports: [SearchComponent],
+  imports: [SearchComponent,NgFor],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
