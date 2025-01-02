4727152fdaf09236e4759b89938b4cc6d170287d
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2025-01-02 14:26:01 +0530

    done:vid024

--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -1,11 +1,11 @@
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FormsModule, NgIf],
+  imports: [FormsModule, NgIf,NgClass],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
